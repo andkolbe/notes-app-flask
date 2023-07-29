4bc1565d10a3185906bdefddf3a4556169ed0d47
Andrew <kolbe1129@gmail.com>
2023-07-29 16:05:40 -0500

    added post sign up code

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request as req, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+    data = req.form
+    print(data)
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
-    return '<p>Sign up</p>'
+    if req.method == 'POST':
+        email = req.form.get('email')
+        firstName = req.form.get('firstName')
+        password1 = req.form.get('password1')
+        password2 = req.form.get('password2')
+
+        if len(email) > 4:
+            flash('email must be greater than 4 characters', category='error')
+        elif len(firstName) < 2:
+            flash('first name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('passwords dont\'t match', category='error')
+        elif len(password1) < 7:
+            flash('password must be at least 7 characters', category='error')
+        else:
+            flash('Account created!', category='success')
+            
+    return render_template('sign_up.html')
